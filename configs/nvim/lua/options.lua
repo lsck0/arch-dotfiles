414ddd4bb014ace5c8ec24672a9a84068cc12467
Luca Sandrock <luca.sandrock@proton.me>
2025-04-25 12:17:39 +0200

    Generation: 2

--- configs/nvim/lua/options.lua
+++ configs/nvim/lua/options.lua
@@ -24,3 +24,19 @@ set.termguicolors = true
 set.timeout = true
 set.timeoutlen = 400
 set.wrap = false
+
+-- wsl clipboard sync
+if vim.fn.has('wsl') == 1 then
+    vim.g.clipboard = {
+        name = 'WslClipboard',
+        copy = {
+            ['+'] = 'clip.exe',
+            ['*'] = 'clip.exe',
+        },
+        paste = {
+            ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+            ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+        },
+        cache_enabled = 0,
+    }
+end
