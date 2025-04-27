01b281690698d1650dc78afc99fc36566c3fb107
Luca Sandrock <luca.sandrock@proton.me>
2025-04-27 04:28:35 +0200

    Generation: 6

--- configs/xdg/link.sh
+++ configs/xdg/link.sh
@@ -2,5 +2,6 @@
 
 set -ex
 
+mkdir -p ${HOME}/documents ${HOME}/downloads ${HOME}/music ${HOME}/pictures ${HOME}/videos ${HOME}/code
 ln -sf ${PWD}/mimeapps.list ${HOME}/.config/mimeapps.list
 ln -sf ${PWD}/user-dirs.dirs ${HOME}/.config/user-dirs.dirs
