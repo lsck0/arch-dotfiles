604fd4e2d1cc2fee82232071419c2aae677b7cea
Luca Sandrock <luca.sandrock@proton.me>
2025-05-30 04:39:07 +0200

    Generation: 25

---
+++ configs/nvim/lua/plugins/lean.lua
@@ -0,0 +1,13 @@
+return {
+    {
+        'Julian/lean.nvim',
+        event = { 'BufReadPre *.lean', 'BufNewFile *.lean' },
+        dependencies = {
+            { 'neovim/nvim-lspconfig' },
+            { 'nvim-lua/plenary.nvim' },
+        },
+        opts = {
+            mappings = true,
+        }
+    }
+}
--- scripts/system-update.sh
+++ scripts/system-update.sh
@@ -3,7 +3,14 @@
 
 set -xe
 
-yay -Syyu
+echo "ARE U SURE?"
+read -p "Type 'y' to continue: " confirm
+if [ "$confirm" != "y" ]; then
+    echo "Aborting update."
+    exit 1
+fi
+
+yay -Syyu --noconfirm
 nix-channel --update
 rustup update
 cargo install-update -a
