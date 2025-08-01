eb3b5a62524a4cab2a1811af7400ba18633da6a3
Luca Sandrock <luca.sandrock@proton.me>
2025-08-01 20:03:38 +0200

    Generation: 53

--- configs/link.sh
+++ configs/link.sh
@@ -9,4 +9,4 @@ ln -sf ${PWD}/nvim ${HOME}/.config/nvim
 ln -sf ${PWD}/rofi ${HOME}/.config/rofi
 ln -sf ${PWD}/waybar ${HOME}/.config/waybar
 ln -sf ${PWD}/wlogout ${HOME}/.config/wlogout
-ln -sf ${PWD}/wofi${HOME}/.config/wofi
+ln -sf ${PWD}/wofi ${HOME}/.config/wofi
