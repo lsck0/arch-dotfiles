49f24b04dec62d7efd88f99a9dcbd071b6ba4d25
Luca Sandrock <luca.sandrock@proton.me>
2025-08-23 02:06:10 +0200

    Generation: 99

--- keyboard/build.sh
+++ keyboard/build.sh
@@ -6,6 +6,6 @@ CWD=$(pwd)
 
 cd ~/code/qmk_firmware/
 qmk compile -e CONVERT_TO=promicro_rp2040 -kb sofle_choc -km luca_sofle_choc
-mv ~/code/qmk_firmware/.build/sofle_choc_luca_sofle_choc_promicro_rp2040.uf2 ~/.dotfiles/keyboard/sofle_choc.uf2
+mv ~/code/qmk_firmware/.build/sofle_choc_luca_sofle_choc_promicro_rp2040.uf2 ~/code/arch-dotfiles/keyboard/sofle_choc.uf2
 
 cd $CWD
