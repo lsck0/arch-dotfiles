fd8ccf5461b9dd6ef89155fea8947909f0b30cc1
Luca Sandrock <luca.sandrock@proton.me>
2025-09-08 17:59:25 +0200

    Generation: 116

--- configs/systemd/link.sh
+++ configs/systemd/link.sh
@@ -2,7 +2,11 @@
 
 set -ex
 
+systemctl --user enable pipewire-pulse.service
+systemctl --user enable pipewire-pulse.socket
+
 sudo systemctl enable avahi-daemon.service
+sudo systemctl enable bluetooth.target
 sudo systemctl enable cronie.service
 sudo systemctl enable cups
 sudo systemctl enable ly.service
