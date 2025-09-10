e15068ffbc092a748ffc308cb008840023d3d6b1
Luca Sandrock <luca.sandrock@proton.me>
2025-09-10 21:00:58 +0200

    Generation: 123

--- configs/link.sh
+++ configs/link.sh
@@ -7,8 +7,6 @@ ln -sf ${PWD}/ghostty ${HOME}/.config/ghostty
 ln -sf ${PWD}/hyprland ${HOME}/.config/hypr
 ln -sf ${PWD}/nsxiv ${HOME}/.config/nsxiv
 ln -sf ${PWD}/nvim ${HOME}/.config/nvim
-ln -sf ${PWD}/rofi ${HOME}/.config/rofi
 ln -sf ${PWD}/walker ${HOME}/.config/walker
 ln -sf ${PWD}/waybar ${HOME}/.config/waybar
 ln -sf ${PWD}/wlogout ${HOME}/.config/wlogout
-ln -sf ${PWD}/wofi ${HOME}/.config/wofi
