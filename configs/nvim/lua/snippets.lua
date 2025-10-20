d6af39ed0448092328ea29ce84a92d362ccf4518
Luca Sandrock <luca.sandrock@proton.me>
2025-10-20 20:39:04 +0200

    Generation: 155

--- configs/nvim/lua/snippets.lua
+++ configs/nvim/lua/snippets.lua
@@ -3,6 +3,19 @@ local s = ls.snippet
 local i = ls.insert_node
 local t = ls.text_node
 
+local common_snippets = {
+    s("banner", {
+        t({ "", "" }),
+        t({ "/*",
+            " * ─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────" }),
+        t({ "", " * " }), i(1),
+        t({ "",
+            " * ─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────",
+            " */" }),
+        t({ "", "" }),
+    }),
+}
+
 ls.add_snippets("rust", {
     s("struct", {
         t("#[derive(Debug, Clone)]"),
@@ -19,3 +32,8 @@ ls.add_snippets("rust", {
         t("}"),
     })
 })
+
+ls.add_snippets("c", vim.deepcopy(common_snippets))
+ls.add_snippets("cpp", vim.deepcopy(common_snippets))
+ls.add_snippets("h", vim.deepcopy(common_snippets))
+ls.add_snippets("hpp", vim.deepcopy(common_snippets))
