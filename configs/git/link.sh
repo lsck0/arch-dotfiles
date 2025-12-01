0514a0a8a2dccd9dd23b1784dcbb8a87f0ff3649
Luca Sandrock <luca.sandrock@proton.me>
2025-12-01 10:19:55 +0100

    Generation: 167

--- configs/git/link.sh
+++ configs/git/link.sh
@@ -5,3 +5,4 @@ set -ex
 git config --global user.email "luca.sandrock@proton.me"
 git config --global user.name "Luca Sandrock"
 git config --global credential.helper store
+git config --global init.defaultBranch master
