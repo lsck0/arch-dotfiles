9c04254678dc061c0e55abb053bda827473b7cc3
Luca Sandrock <luca.sandrock@proton.me>
2026-01-01 18:35:58 +0100

    Generation: 197

--- scripts/system-update.sh
+++ scripts/system-update.sh
@@ -3,7 +3,14 @@
 
 set -e
 
-arch-update
+echo "ARE U SURE?"
+read -p "Type 'y' to continue: " confirm
+if [ "$confirm" != "y" ]; then
+    echo "Aborting update."
+    exit 1
+fi
+
+yay -Syyu --noconfirm --rebuildall --answerclean A
 flatpak update --assumeyes
 nix-channel --update
 rustup update
