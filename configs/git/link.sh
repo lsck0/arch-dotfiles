1a73f65d6c9818b6d862c4c7f66fabceb547dd71
Luca Sandrock <luca.sandrock@proton.me>
2026-02-09 16:43:35 +0100

    Generation: 210

--- configs/git/link.sh
+++ configs/git/link.sh
@@ -6,3 +6,4 @@ git config --global user.email "luca.sandrock@proton.me"
 git config --global user.name "Luca Sandrock"
 git config --global credential.helper store
 git config --global init.defaultBranch master
+git config --global pull.rebase true
--- configs/ollama/link.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -ex
-
-sudo systemctl enable --now ollama.service
