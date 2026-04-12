f083dbd7efd6a073355531179e075f54d0c6cd47
Luca Sandrock <luca.sandrock@proton.me>
2026-04-12 21:54:42 +0200

    Generation: 259

--- configs/ssh/link.sh
+++ configs/ssh/link.sh
@@ -6,5 +6,3 @@ sudo systemctl enable sshd
 
 mkdir -p ${HOME}/.config/systemd/user
 ln -sf ${PWD}/ssh-agent.service ${HOME}/.config/systemd/user/ssh-agent.service
-
-systemctl enable --user ssh-agent.service
