60b69b2d7fa27ce4824207221a2bdbd463571b38
Luca Sandrock <luca.sandrock@proton.me>
2026-05-01 19:27:36 +0200

    Generation: 271

--- configs/discord/link.sh
+++ configs/discord/link.sh
@@ -13,12 +13,9 @@ ln -sf ${PWD}/discord_settings.json ${HOME}/.config/discord/settings.json
 cp ${PWD}/wal.theme.css ${HOME}/.config/BetterDiscord/themes/wal.theme.css
 
 # download plugins
-wget "https://github.com/JustOptimize/ShowHiddenChannels/releases/download/v0.6.4/ShowHiddenChannels.plugin.js" -O ${HOME}/.config/BetterDiscord/plugins/ShowHiddenChannels.plugin.js
+wget "https://github.com/JustOptimize/ShowHiddenChannels/releases/download/v0.6.8/ShowHiddenChannels.plugin.js" -O ${HOME}/.config/BetterDiscord/plugins/ShowHiddenChannels.plugin.js
 wget "https://raw.githubusercontent.com/1Lighty/BetterDiscordPlugins/refs/heads/master/Plugins/MessageLoggerV2/MessageLoggerV2.plugin.js" -O ${HOME}/.config/BetterDiscord/plugins/MessageLoggerV2.plugin.js
-wget "https://raw.githubusercontent.com/Colonial-Dev/WayAFK/refs/heads/master/WayAFK.plugin.js" -O ${HOME}/.config/BetterDiscord/plugins/WayAFK.plugin.js
 wget "https://raw.githubusercontent.com/Farcrada/DiscordPlugins/7f1c3f98461bcf1b336c2df6e28ca025d09d03cb/Double-click-to-edit/DoubleClickToEdit.plugin.js" -O ${HOME}/.config/BetterDiscord/plugins/DoubleClickToEdit.plugin.js
-wget "https://raw.githubusercontent.com/TheCommieAxolotl/BetterDiscord-Stuff/1b9a38609200d08886be9bba7f4df425eafbf561/Timezones/Timezones.plugin.js" -O ${HOME}/.config/BetterDiscord/plugins/Timezones.plugin.js
-wget "https://raw.githubusercontent.com/TheCommieAxolotl/BetterDiscord-Stuff/38b22daa8db787b24a60730e51cfd0f757949704/BetterSyntax/BetterSyntax.plugin.js" -O ${HOME}/.config/BetterDiscord/plugins/BetterSyntax.plugin.js
 wget "https://raw.githubusercontent.com/TheLazySquid/BetterDiscordPlugins/d4ff127db89318d2361086e0e85251fcc72e4fed/plugins/ZipPreview/ZipPreview.plugin.js" -O ${HOME}/.config/BetterDiscord/plugins/ZipPreview.plugin.js
 wget "https://raw.githubusercontent.com/binarynoise/open-in-mpv/acc203b0b4e7813cb072600972a9214f70b6ed73/open-in-mpv.plugin.js" -O ${HOME}/.config/BetterDiscord/plugins/open-in-mpv.plugin.js
 wget "https://raw.githubusercontent.com/mwittrien/BetterDiscordAddons/50d8a379b9e98e114a3e4f20871ae1aaea13ab8d/Plugins/LastMessageDate/LastMessageDate.plugin.js" -O ${HOME}/.config/BetterDiscord/plugins/LastMessageDate.plugin.js
