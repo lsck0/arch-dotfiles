735e2bacaac493aa4c7e9e6a22bd83337d046daa
Luca Sandrock <luca.sandrock@proton.me>
2026-05-13 03:58:26 +0200

    Generation: 293

--- configs/hypr/manual_link.sh
+++ configs/hypr/manual_link.sh
@@ -4,9 +4,11 @@ set -ex
 
 yes | hyprpm update -f
 
-yes | hyprpm add https://github.com/hyprwm/hyprland-plugins
 yes | hyprpm add https://github.com/0xFMD/hyprmodoro
+yes | hyprpm add https://github.com/KZDKM/Hyprspace
+yes | hyprpm add https://github.com/hyprnux/hyprglass
+yes | hyprpm add https://github.com/hyprwm/hyprland-plugins
+yes | hyprpm add https://github.com/outfoxxed/hy3
 yes | hyprpm add https://github.com/virtcode/hypr-dynamic-cursors
 
 hyprpm enable dynamic-cursors
-hyprpm enable hyprexpo
