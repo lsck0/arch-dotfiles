48f7cd16b5183ef3e56165bf590df7508790ba59
Luca Sandrock <luca.sandrock@proton.me>
2026-06-03 23:31:15 +0200

    Generation: 318

--- configs/locale/link.sh
+++ configs/locale/link.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-# enable german locale
 sudo sed -i 's/^# *\(de_DE\.UTF-8 UTF-8\)/\1/' /etc/locale.gen
+sudo sed -i 's/^# *\(en_US\.UTF-8 UTF-8\)/\1/' /etc/locale.gen
 
 sudo locale-gen
