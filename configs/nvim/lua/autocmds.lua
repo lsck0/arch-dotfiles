6c924c86b48447a97bfc4ab1164ebe86c85f021d
Luca Sandrock <luca.sandrock@proton.me>
2026-06-08 01:46:47 +0200

    Generation: 322

--- configs/nvim/lua/autocmds.lua
+++ configs/nvim/lua/autocmds.lua
@@ -8,3 +8,16 @@ autocmd("TextYankPost", {
         vim.highlight.on_yank()
     end,
 })
+
+autocmd("FileType", {
+    desc = "Start treesitter highlighting + indent",
+    group = group("treesitter-start", { clear = true }),
+    callback = function(args)
+        local buf = args.buf
+        local ft = vim.bo[buf].filetype
+        local lang = vim.treesitter.language.get_lang(ft) or ft
+        if pcall(vim.treesitter.start, buf, lang) then
+            vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end
+    end,
+})
