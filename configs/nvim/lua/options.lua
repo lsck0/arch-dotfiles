ffc8c529505d403b8bcc9758a1615aac86e2c209
Luca Sandrock <luca.sandrock@proton.me>
2026-07-02 00:17:20 +0200

    Generation: 350

--- configs/nvim/lua/options.lua
+++ configs/nvim/lua/options.lua
@@ -6,7 +6,7 @@ set.colorcolumn = "120"
 set.cursorline = true
 set.expandtab = true
 set.fillchars = { eob = " " }
-set.guifont = "0xProto Nerd Font:h22"
+set.guifont = "0xProto Nerd Font:h16"
 set.ignorecase = true
 set.inccommand = "split"
 set.laststatus = 3
@@ -36,3 +36,7 @@ set.updatetime = 250
 set.virtualedit = "block"
 set.winborder = "rounded"
 set.wrap = false
+
+if vim.g.neovide then
+    vim.g.neovide_scale_factor = 1.0
+end
