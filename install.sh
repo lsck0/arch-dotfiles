e4a813bf8a5607119c51608852ed8fb6712107c6
Luca Sandrock <luca.sandrock@proton.me>
2026-07-19 22:27:34 +0200

    Generation: 365

--- install.sh
+++ install.sh
@@ -737,6 +737,11 @@ sudo pacman-key --init
 sudo pacman-key --populate archlinux
 sudo pacman -Syyu --noconfirm
 
+# chaotic aur
+sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key 3056513887B78AEB
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm
+
 # install yay
 sudo pacman -S --needed --noconfirm git base-devel && \
     git clone https://aur.archlinux.org/yay.git && \
